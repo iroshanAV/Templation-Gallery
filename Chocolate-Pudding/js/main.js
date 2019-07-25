0c8a0b869854fc8c42fd9f584936894d4d3c0c68
Iroshan <iroshanav@gmail.com>
2019-07-25 09:55:47 +0530

    responsiveness

---
+++ Chocolate-Pudding/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready (function() {
+        
+    
+    $(".burger-nav").on("click", function(){
+        
+        $(".container nav ul").toggleClass("open");
+        
+    });
+    
+        
+    
+    
+});
